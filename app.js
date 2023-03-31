80fbbc4f241189312cd1fb769a45d7fa10b6bf49
melvinf21 <melvinfontanez@gmail.com>
2023-03-31 16:35:56 -0400

    Add Shuffle Function

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 // Declare variables
-let deck1 = []
-let deck2 = []
-let deck3 = []
-let deck4 = []
+let playerDeck = []
+let computerDeck = []
+let warzone 
 
-let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+let gameDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 
-let cardToRemove
 
-// Cached element references
+
+// Cached element references \\
 let deck1El = document.getElementById('deck-1')
 let deck2El = document.getElementById('deck-2')
 let deck3El = document.getElementById('deck-3')
 let deck4El = document.getElementById('deck-4')
 
-// Event listeners
-document.getElementById('btn').addEventListener('click', handleClickPlayer1)
-document.getElementById('btn2').addEventListener('click', handleClickPlayer2)
-// Functions
-initDeck1()
-initDeck4()
+// // Event listeners
+// document.getElementById('btn').addEventListener('click', handleClickPlayer1)
+// document.getElementById('btn').addEventListener('click', handleClickPlayer1)
+// // document.getElementById('btn2').addEventListener('click', handleClickPlayer2)
+// // Functions
 
-console.log(initDeck1);
-console.log(initDeck4);
 
-// Initialize deck 1 with array of 52 cards
-function initDeck1() {
-  deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+
+function initDecks() {
+  shuffleDeck()
+  splitDeck()
+  
+}
+console.log(initDecks);
+
+function shuffleDeck(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-function initDeck4(){
-  deck4 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+shuffleDeck(gameDeck)
+console.log(gameDeck);
+
+function splitDeck(){
+  shuffleDeck(gameDeck)
+  
 }
 
 function handleClickPlayer1() {
@@ -132,9 +142,4 @@ function renderPlayer2(cardPicked) {
     deck4El.classList.add("outline");
     deck4El.classList.remove("back-blue");
   }
-}
-
-function cardValueAssignments{
-  initDeck1()
-  if deck1
 }
