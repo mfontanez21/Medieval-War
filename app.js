80e842e8611f47ecabce7cb78e2fd2bd7b32661a
melvinf21 <melvinfontanez@gmail.com>
2023-03-31 09:48:49 -0400

    Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let map, playerDeck, computerDeck
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+function initialize()
+
+function playACard()
+
+function goToWar()
+
+function render()
+
+function createDeck() {
+	let deck = [];
+	forEach.deck(function() =>{
+    deck[i] = i;
+  }) 
+}
